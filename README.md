2ce4f2bfd0e158bb74ee57ca04635adbdcff52c8
Amish Shah <amishshah.2k@gmail.com>
2017-06-03 15:44:39 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cah-cards
-The normal Cards Against Humanity cards
+Cards Against Humanity but in JSON (for usage with Crude Cards)
+
+Thanks to [Cards Against Humanity](https://cardsagainsthumanity.com/) for being the entire purpose of this repository - go buy their cards! Cards Against Humanity's work is licensed under the [BY-NC-SA 2.0 Creative Commons](https://creativecommons.org/licenses/by-nc-sa/2.0/) license.
+
+Also, thanks to [Chris Hallberg](https://www.crhallberg.com/cah/json/) for providing a service that gives JSON data for Cards Against Humanity decks!
---
+++ src/download.js
@@ -0,0 +1,78 @@
+const request = require('superagent');
+const fs = require('fs');
+
+const decks = [
+	'Base',
+	'CAHe1',
+	'CAHe2',
+	'CAHe3',
+	'CAHe4',
+	'CAHe5',
+	'CAHe6',
+	'greenbox',
+	'90s',
+	'Box',
+	'fantasy',
+	'food',
+	'science',
+	'www',
+	'hillary',
+	'trumpvote',
+	'trumpbag',
+	'xmas2012',
+	'xmas2013',
+	'PAXE2013',
+	'PAXP2013',
+	'PAXE2014',
+	'PAXEP2014',
+	'PAXPP2014',
+	'PAX2015',
+	'HOCAH',
+	'reject',
+	'reject2',
+	'Canadian',
+	'misprint'
+];
+
+console.log(`${Object.keys(decks).length} decks...`);
+
+const form = decks.map(v => `decks[]=${v}`).join('&') + '&type=JSON';
+const successful = [];
+
+request
+	.post('https://www.crhallberg.com/cah/json/output.php')
+	.type('form')
+	.send(form)
+	.then(res => {
+		if (!res.body) throw Error('no body');
+		for (const deck of decks) {
+			const parsed = { cards: [] };
+			const obj = res.body[deck];
+			parsed.name = obj.name;
+			parsed.cards = parsed.cards.concat(
+				obj.black.map(v => ({
+					type: 0,
+					content: res.body.blackCards[v].text
+				}))
+			);
+			parsed.cards = parsed.cards.concat(
+				obj.white.map(v => ({
+					t: 1,
+					content: res.body.whiteCards[v]
+				}))
+			);
+			fs.writeFile(`./src/cards/${deck}.json`, JSON.stringify(parsed), e => {
+				decks.pop();
+				if (e) console.error(e);
+				else successful.push(deck);
+				if (decks.length === 0) genIndex();
+			});
+		}
+	})
+	.catch(console.error);
+
+	function genIndex() {
+		const exp = successful.map(d => `'${d}': require('./cards/${d}')`);
+		const template = `module.exports = {${exp.join(',')}}`;
+		fs.writeFile('./src/map.js', template, console.log);
+	}
